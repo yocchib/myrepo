bfd1d0565ca94383bd236820e5800f7a61122927
taka-PC\taka <yocchib@gmail.com>
2016-11-06 20:57:48 +0900

    nikkeiBP: クロージャ注意事項

--- js/nikkeiBP.js
+++ js/nikkeiBP.js
@@ -1,12 +1,12 @@
 var man = { name : "Yamada", age : 18, sex : 'Male', foo: "bar" }
 var keys = Object.keys(man)
 for (var i = 0; i < keys.length ; i++) {
- console.log(keys[i]);
+ // console.log(keys[i]);
 }
 console.log("\n");
 
 for (var key in man) {
- console.log(man[key]);
+ // console.log(man[key]);
 }
 
 /* 45頁 : 即時関数 
@@ -26,13 +26,14 @@ function sumAppliedBValues(f, x, y) {
 }
 
 result = sumAppliedBValues(toDouble, 1,2)
-console.log(result );
-console.log("");
+// console.log(result );
+// console.log("");
 
 /* 46頁 : クロージャの例
  * クロージャのメリットは２つ
  *   (1) グローバル変数を使わなくて済む
- *   (2) 関数を動的に生成できる
+ *   (2) 関数を動的に生成できる … 外側の関数は「関数を作り出す関数」
+ *        外側の関数に与える引数に応じて様々な関数を生成できる
  */
 function outer() {
   var a = 2;
@@ -51,3 +52,14 @@ console.log(result );
 result = func();
 console.log(result );
 
+/* 48頁 : クロージャのスコープに注意
+ */
+var names = ['Yamada', 'Takahashi', 'Sato'];
+var callNames = [];
+for (var i=0; i < 3; i++)  {
+  // callNames[i] = function(){ return names[i]};
+  callNames[i] = (function(j) { return function () { return names[j]; } } )(i);
+}
+console.log( callNames[0]());
+console.log( callNames[1]());
+console.log( callNames[2]());
