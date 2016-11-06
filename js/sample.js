68c05cdfb24f174a423605c896e45a73cd6973ec
NEC-PCuser-PC\taka <yocchib@gmail.com>
2016-11-06 13:58:15 +0900

    no message

---
+++ js/sample.js
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+var obj = require('./ch8_module.js') ;
+
+obj.add() ;
+obj.set1(3).set2(4).add()  // 7 // メソッドチェーン利用例
+obj.result
+
+obj.o.m();
+obj.max(1,3,5,7,9,2,4,6);  // 9
+var i = obj.uniqInteger();
+
+
