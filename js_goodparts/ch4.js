5088db97ef38dc043344279fb9b4bdbd340c0838
DESKTOP-ITTJMFV\z91002 <yocchib@mineo.ne.jp>
2016-12-22 16:45:52 +0900

    no message

--- js_goodparts/ch4.js
+++ js_goodparts/ch4.js
@@ -112,17 +112,23 @@
   // prototypeプロパティを直接指定せずにメソッドを追加できるため
   // 可読性がよい
   Function.prototype.method = function(name, func) {
-     this.prototype[name] = func;
-     return this;
+     if (!this.prototype[name] ) {
+       this.prototype[name] = func;
+       return this;
+     }
   };
   // Math.ceil(数字)  数字を切り上げして整数にします
   // Math.floor(数字)小数以下を切り捨てして整数にします
   Number.method('integer', function(){
     return Math[this < 0 ? 'ceil' : 'floor'](this);
   });
-
   console.log((-10/3));
   console.log((-10/3).integer());
+
+  String.method('trim', function(){
+    return this.replace(/^\s+|\s+$/g, '');
+  });
+  console.log('"' + "  neat  ".trim() + '"');
 
 })();
 
