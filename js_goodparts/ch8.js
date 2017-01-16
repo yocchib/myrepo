86b72924b823ae1814ab3b65dfec4bac143a9704
DESKTOP-ITTJMFV\z91002 <yocchib@mineo.ne.jp>
2017-01-16 16:42:20 +0900

    ch8 sort

--- js_goodparts/ch8.js
+++ js_goodparts/ch8.js
@@ -11,7 +11,20 @@
   n.sort( function(a, b) {
     return a - b;
   });
-  console.log(n);
+  console.log("n: " + n);
+
+  // 単純な値の汎用ソート関数
+  var m = ['aa', 'bb', 'a', 23, 4, 16, 42, 8];
+  m.sort(function(a, b){
+    if (a===b) {
+      return 0;
+    }
+    if (typeof a === typeof b) {
+      return a < b ? -1 : 1;
+    }
+    return typeof a < typeof b ? -1 : 1;
+  });
+  console.log("m: " + m);
 
   // メンバ名を引数にして比較関数を返す by関数
   var by = function(name) {
@@ -36,11 +49,43 @@
     };
   };
   var s = [
-    {first: 'Joe', last: 'Besser'},
-    {first: 'Moe', last: 'Howard'},
+    {first: 'Joe',   last: 'Besser'},
+    {first: 'Moe',   last: 'Howard'},
+    {first: 'Joe',   last: 'DeRita'},
+    {first: 'Shemp', last: 'Howard'},
+    {first: 'Larry', last: 'Fine'  },
+    {first: 'Curly', last: 'Howard'},
   ];
-  s.sort(by('first'));
+  // s.sort(by('first'));
+  // s.map( function(param) { console.log(param) });
   
+  // by関数を複数パラメータでソートできるように改造
+  var byKeys = function(name, minor) {
+    return function(o, p) {
+      var a, b;
+      if (typeof o === 'object' && typeof p === 'object' && o && p) {
+          a = o[name];
+          b = p[name];
+          if (a === b) {
+            // return 0;
+            return typeof minor === 'function' ? minor(o, p) : 0;
+          }
+          if (typeof a === typeof b) {
+            return a < b ? -1 : 1;
+          }
+          return typeof a < typeof b ? -1:1;
+      } else {
+          throw {
+              name: 'Error',
+              message: name + 'を含むオブジェクトが必要!'
+          };
+      }
+    };
+  };
+  // s.sort(byKeys('first', byKeys('last')));
+  s.sort(byKeys('last', byKeys('first')));
+  console.log("複数キーでのソート：");
+  s.map( function(param) { console.log(param) });
 
 })();
 
